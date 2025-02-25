f4abc54d922546e541bc7a725fa34a5179e21633
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2025-02-25 16:13:23 -0700

    checkWinner() functionality wokring!

--- main.cpp
+++ main.cpp
@@ -86,10 +86,36 @@ void computerMove(char *spaces, char computer) {
 
 bool checkWinner(char *spaces, char player, char computer) {
 
-    if (spaces[0] != ' ' && spaces[0] == spaces[1] && spaces[1] == spaces[2]) {
+    // Checks for row wins
+    if ((spaces[0] != ' ') && (spaces[0] == spaces[1]) && (spaces[1] == spaces[2])) {
         spaces[0] == player ? cout << "YOU WIN!\n" : cout << "YOU LOSE!\n";
     }
-
+    else if ((spaces[3] != ' ') && (spaces[3] == spaces[4]) && (spaces[4] == spaces[5])) {
+        spaces[3] == player ? cout << "YOU WIN!\n" : cout << "YOU LOSE!\n";
+    }
+    else if ((spaces[6] != ' ') && (spaces[6] == spaces[7]) && (spaces[7] == spaces[8])) {
+        spaces[6] == player ? cout << "YOU WIN!\n" : cout << "YOU LOSE!\n";
+    }
+    // Checks for column wins
+    else if ((spaces[0] != ' ') && (spaces[0] == spaces[3]) && (spaces[3] == spaces[6])) {
+        spaces[0] == player ? cout << "YOU WIN!\n" : cout << "YOU LOSE!\n";
+    }
+    else if ((spaces[1] != ' ') && (spaces[1] == spaces[4]) && (spaces[4] == spaces[7])) {
+        spaces[1] == player ? cout << "YOU WIN!\n" : cout << "YOU LOSE!\n";
+    }
+    else if ((spaces[2] != ' ') && (spaces[2] == spaces[5]) && (spaces[5] == spaces[8])) {
+        spaces[2] == player ? cout << "YOU WIN!\n" : cout << "YOU LOSE!\n";
+    }
+    // diagonals
+    else if ((spaces[0] != ' ') && (spaces[0] == spaces[4]) && (spaces[4] == spaces[8])) {
+        spaces[0] == player ? cout << "YOU WIN!\n" : cout << "YOU LOSE!\n";
+    }
+    else if ((spaces[2] != ' ') && (spaces[2] == spaces[4]) && (spaces[4] == spaces[6])) {
+        spaces[2] == player ? cout << "YOU WIN!\n" : cout << "YOU LOSE!\n";
+    }
+    else {
+        return false;
+    }
 
     return true;
 }
