8805c692c6d3aa52b6c1d7875fdaf896d8896184
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2025-02-25 15:08:20 -0700

    updating to current spot in building out our main and functions

---
+++ main.cpp
@@ -0,0 +1,43 @@
+
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+// MARK: - DECLARING FUNCTIONS:
+
+void drawBoard(char *spaces);
+void playerMove(char * spaces, char player);
+void computerMove(char *spaces, char computer);
+bool checkWinner(char *spaces, char player, char computer);
+bool checkTie(char *spaces);
+
+
+// MARK: - MAIN FUNCTION
+int main() {
+
+
+    return 0;
+}
+
+// MARK: -
+
+void drawBoard(char *spaces) {
+
+}
+
+void playerMove(char *spaces, char player) {
+
+}
+
+void computerMove(char *spaces, char computer) {
+
+}
+
+bool checkWinner(char *spaces, char player, char computer) {
+
+}
+
+bool checkTie(char *spaces) {
+    
+}
